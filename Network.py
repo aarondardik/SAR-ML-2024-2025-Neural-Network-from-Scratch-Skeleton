ac2afd15e113ac21e93199bffa31ffd48212127c
aarondardik <50251249+aarondardik@users.noreply.github.com>
2024-12-09 11:02:38 -0500

    Create Network.py

---
+++ Network.py
@@ -0,0 +1,21 @@
+import random
+import numpy as np
+
+
+#Network class
+class Network(object):
+  def __init__(self, sizes):
+    #fill in
+    #self.numLayers =
+    #self.sizes = #
+    #self.biases =
+    #self.weights =
+
+  #feedforward function
+  def feedforward(self, input):
+    #fill in
+
+#Note that in this implementation I put sigmoid and sigmoid prime outside the network class (so I don't need to write "self" in the function params)
+#def sigmoid(z):
+
+#def sigmoid_prime(z):
